1e8f427ca5a9735daf4596ab331b932d487f9ac9
Manoel Pedro <manoeltelesdev@gmail.com>
2025-09-03 10:17:18 -0300

    feat: atividade de CRUD em aula

---
+++ Aulas/aula_2/manipulacaoLivraria.sql
@@ -0,0 +1,26 @@
+use Livraria;
+
+insert into Autor (Nome)
+value
+("J. K. Rowling"), ("Yoshihiro Togashi");
+
+insert into Livro(ISBN, Titulo, Ano, ID_autor)
+values 
+("9788869183157", "Harry Potter and the Philosopher's Stone", 1997, 4), ("9788831000154", "Harry Potter and the Chamber of Secrets", 2002, 4),
+("8577870405", "Hunter x Hunter", 1998, 5), ("8577870406","Yu Yu Hakusho", 1990, 5);
+
+insert into Pessoa(CPF, Nome, Data_nasc)
+values
+("51829643007", "Tiago", "2003-05-09"), ("30217498561", "Pietro", "2016-09-01");
+
+select * from Pessoa;
+
+insert into Emprestimo(ISBN_livro, ID_pessoa)
+values
+('9788869183157', 2),
+('8577870405', 1),
+('8577870406', 3);
+
+
+
+
